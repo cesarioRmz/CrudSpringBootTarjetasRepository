6b586111826b46e3bb2f659d2c82a4f0252d50e8
CESARIO RAMIREZ TORRES <rmzmoralesazul@gmail.com>
2023-04-17 21:02:47 -0600

    subiendo proyecto base

---
+++ src/main/java/com/CrudSpringBootTarjetasRepository/CrudSpringBootTarjetasRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.CrudSpringBootTarjetasRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootTarjetasRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootTarjetasRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CrudSpringBootTarjetasRepository/CrudSpringBootTarjetasRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrudSpringBootTarjetasRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootTarjetasRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
